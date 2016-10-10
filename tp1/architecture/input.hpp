7d09882f86d6473395bd3048d7e5e436437beb6d
begarco <benoit.garcon@outlook.com>
2016-10-10 12:04:03 +0200

    3eme seance

---
+++ tp1/architecture/input.hpp
@@ -0,0 +1,65 @@
+/**
+    Secrets of Universe
+    Copyright (C) 2014  BG Prod
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact me : bgprod@outlook.com
+**/
+
+#ifndef INPUT_H
+#define INPUT_H
+
+#include "gui.hpp"
+
+#define X                   0
+#define Y                   1
+#define X_REEL              2
+#define Y_REEL              3
+#define SDLK_SIZE				 1000
+
+class Input
+{
+    public:
+        Input();
+        virtual ~Input();
+
+        void update();
+        /// getter
+        char get_touche(int i){return touche[i];}
+        char get_souris_boutons(int i){return souris_boutons[i];}
+        char get_exit(){return exit;}
+        int mouse(int i){return souris[i];}
+        /// setter
+        void set_touche(int i, int valeur){touche[i] = (char)valeur;}
+        void set_souris_boutons(int i, int valeur){souris_boutons[i] = (char)valeur;}
+        void set_exit(int valeur){exit = (char)valeur;}
+        void set_souris(int i, int valeur){souris[i] = valeur;}
+
+        /// saisie
+        void activerSaisie();
+        std::string str();
+        void getSaisie();
+        void desactiverSaisie();
+        bool modeSaisie();
+
+    protected:
+        char touche[SDLK_SIZE], souris_boutons[8], exit;
+        int souris[4];
+        std::string m_str;
+        bool saisieActive;
+};
+
+#endif /// INPUT_H
+
